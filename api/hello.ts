af76c728b4dcdba5ac0b57852bb0f735908bd713
Franck MICHAUD <frmi2018@gmail.com>
2025-05-25 00:22:00 +0200

    Fix vercel config and handler

---
+++ api/hello.ts
@@ -0,0 +1,8 @@
+import type { IncomingMessage, ServerResponse } from 'http';
+
+export default function handler(req: IncomingMessage, res: ServerResponse) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify({ message: 'Hello from modern Vercel API!' }));
+}
+
